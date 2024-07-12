5f24d732cd5297e5c30135310b5d300f3a81431d
Saikat12N <saikat.12n@gmail.com>
2024-07-12 11:46:21 +0530

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './component/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React APP</h1>
+    </div>
+  );
+}
+
+export default App;
